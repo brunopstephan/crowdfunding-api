32d76cb42b41ed4b18a15cb0e85fbff686707a02
brunopstephan <jogosdebruno123@gmail.com>
2024-06-27 18:01:11 -0300

    feat(tasks): add closeCrowdfundingsTask task

--- src/tasks/close-crowdfundings.task.ts
+++ src/tasks/close-crowdfundings.task.ts
@@ -1,14 +1,13 @@
-import { Crowdfunding } from '@/schemas'
 import { Postgres } from '@/utils'
 import cron from 'node-cron'
 
 export function closeCrowdfundingsTask(db: Postgres) {
+  //every monday as 4am
   cron.schedule('0 0 4 * * MON', async () => {
-    const { rows: crowdFundingsToClose } = await db.query<Crowdfunding>(
-      'SELECT * FROM crowdfunding WHERE expire_at < NOW() AND closed = $1',
-      [0],
+    await db.query(
+      'UPDATE crowdfundings SET closed = 1 WHERE expire_at IS NOT NULL AND expire_at <= CURRENT_DATE',
     )
 
-    if (!crowdFundingsToClose.length) return
+    return 'ok'
   })
 }
